537b55041142f29013aa3da2c829700c804d1c73
kct285 <kimberlytanchay@Kimberlys-MacBook-Pro.local>
2019-11-09 19:52:05 +0800

    first and half of second section

--- src/App.js
+++ src/App.js
@@ -5,21 +5,148 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header> */}
+
+       <div class="navigation">
+        <div class="nav-title">
+          <nav>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#features">Features</a></li>
+              <li><a href="#support">Support</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
       </header>
-    </div>
+
+       <div class="home">
+
+         <div class="flex">
+           <div>
+         <img src="https://cdn1.imggmi.com/uploads/2019/11/8/4355c1bd8f5fa324f432ff462963d76f-full.png" width="750px" />
+         </div>
+
+         <div id="home_text"> 
+         <h1 class="quotation"> Disrupting <br></br> Disruption.  </h1>
+           <p id="home_desc">Procrastinate from social media.</p>
+
+           <button class="download1"> FREE DOWNLOAD</button> 
+           </div>
+         </div>
+      </div>
+
+
+
+      <div class="features">
+           <div class="flex">
+
+                <div id="featurestxt">
+                     <h2>Features</h2>
+                     <p> sample text</p>
+                </div>
+
+                <div>
+                <img id="featureimg" src="https://i.imgur.com/TarlUAm.jpg" width="900px"/>
+                </div>
+
+                
+
+
+           </div>
+
+      </div>
+
+
+      <div class="support">
+
+
+      </div>
+
+
+
+        {/* <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <a href="index.html" class="navbar-brand">Hydro</a>
+               </div>
+              <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-nav-first">
+                         <li><a href="#home" class="smoothScroll">Home</a></li>
+                         <li><a href="#about" class="smoothScroll">About</a></li>
+                         <li><a href="#blog" class="smoothScroll">Blog</a></li>
+                         <li><a href="#work" class="smoothScroll">Our Work</a></li>
+                         <li><a href="#contact" class="smoothScroll">Contacts</a></li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                         <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                         <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                         <li class="section-btn"><a href="#" data-toggle="modal" data-target="#modal-form">Sign in / Join</a></li>
+                    </ul>
+               </div>
+
+          </div>
+     </section>
+
+     <section id="home" data-stellar-background-ratio="0.5">
+          <div class="overlay"></div>
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="home-info">
+                              <h1>We make beautiful websites for all people.</h1>
+                              <a href="#about" class="btn section-btn smoothScroll">Start a project</a>
+                              <span>
+                                   CALL US (+66) 010-020-0340
+                                   <small>For any inquiry</small>
+                              </span>
+                         </div>
+                    </div>
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="home-video">
+                              <div class="embed-responsive embed-responsive-16by9">
+                                   <iframe src="https://www.youtube.com/embed/AqcjdkPMPJA" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </section> */}
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+  </div>
+
+
+
+
+
+
+    
   );
 }
 
