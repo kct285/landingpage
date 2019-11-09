65bc1476ec5c1851fd5c8bee2b66a8ee84a992ae
kct285 <kimberlytanchay@Kimberlys-MacBook-Pro.local>
2019-11-09 20:59:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -44,6 +44,9 @@ function App() {
                 <div id="featurestxt">
                      <h2>Features</h2>
                      <p> sample text</p>
+                     <img src="https://cdn1.imggmi.com/uploads/2019/11/9/4ea884cfff6734eec9b596688ca534a6-full.png" width="60px"/> <br></br>
+                     <img src="https://cdn1.imggmi.com/uploads/2019/11/9/cfb9fdf24c58ca6dd594bef52152627c-full.png" width="60px"/> <br></br>
+                     <img src="https://cdn1.imggmi.com/uploads/2019/11/9/dc0fe207d699eb6d9e0bec9f2b3db6d7-full.png"width="60px"/> 
                 </div>
 
                 <div>
@@ -55,75 +58,22 @@ function App() {
 
            </div>
 
+           <img id="testimonials" src="https://cdn1.imggmi.com/uploads/2019/11/9/b539fdd05ff614f9700e16582a46d17b-full.png" width="800px" />
+
       </div>
 
 
       <div class="support">
 
+      <button class="download1" id="download2"> DOWNLOAD NOW</button> 
+
+
 
       </div>
 
 
 
-        {/* <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
-          <div class="container">
-
-               <div class="navbar-header">
-                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                    </button>
-
-                    <a href="index.html" class="navbar-brand">Hydro</a>
-               </div>
-              <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-nav-first">
-                         <li><a href="#home" class="smoothScroll">Home</a></li>
-                         <li><a href="#about" class="smoothScroll">About</a></li>
-                         <li><a href="#blog" class="smoothScroll">Blog</a></li>
-                         <li><a href="#work" class="smoothScroll">Our Work</a></li>
-                         <li><a href="#contact" class="smoothScroll">Contacts</a></li>
-                    </ul>
-
-                    <ul class="nav navbar-nav navbar-right">
-                         <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-                         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                         <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                         <li class="section-btn"><a href="#" data-toggle="modal" data-target="#modal-form">Sign in / Join</a></li>
-                    </ul>
-               </div>
-
-          </div>
-     </section>
-
-     <section id="home" data-stellar-background-ratio="0.5">
-          <div class="overlay"></div>
-          <div class="container">
-               <div class="row">
-
-                    <div class="col-md-6 col-sm-12">
-                         <div class="home-info">
-                              <h1>We make beautiful websites for all people.</h1>
-                              <a href="#about" class="btn section-btn smoothScroll">Start a project</a>
-                              <span>
-                                   CALL US (+66) 010-020-0340
-                                   <small>For any inquiry</small>
-                              </span>
-                         </div>
-                    </div>
-
-                    <div class="col-md-6 col-sm-12">
-                         <div class="home-video">
-                              <div class="embed-responsive embed-responsive-16by9">
-                                   <iframe src="https://www.youtube.com/embed/AqcjdkPMPJA" frameborder="0" allowfullscreen></iframe>
-                              </div>
-                         </div>
-                    </div>
-                    
-               </div>
-          </div>
-     </section> */}
+ 
 
 
 
